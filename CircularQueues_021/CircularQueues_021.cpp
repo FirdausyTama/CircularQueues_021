4151f455b5669baec19ed6ae9c5e94239043eaa4
FirdausyTama <atama.firdausy@gmail.com>
2023-06-22 10:47:03 +0700

    jika REAR berada di posisi terakhir array, kembali ke awal array

--- CircularQueues_021/CircularQueues_021.cpp
+++ CircularQueues_021/CircularQueues_021.cpp
@@ -27,5 +27,12 @@ public :
 		if (FRONT == -1) {
 			REAR = 0;
 		}
+		else {
+			// jika REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
 	}
 };
